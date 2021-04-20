24e003d58b94b89e6a6028159968984698ae1625
Nathan Lu <nathanlumax@gmail.com>
2021-04-19 23:37:25 -0500

    added queue implementation

--- queue.cpp
+++ queue.cpp
@@ -5,3 +5,97 @@ Purpose: To create a bi-directional or undirected graph that is weighted, using 
 */
 
 #include "queue.h"
+
+/*
+bool Queue::push(int num){
+    if (top < QUEUE_SIZE - 1){ 
+        QueueArr[++top] = num;
+        return true;
+    } else {
+        throw 1;
+    }
+}
+
+bool Queue::isEmpty(){
+    bool empty = true;
+
+    if(top >= 0){
+        empty = false;
+    }
+
+    return empty;
+}
+
+int Queue::peek(){
+    if(!isEmpty()){
+        return QueueArr[0];
+    } else {
+        throw 1;
+    }
+}
+
+int Queue::(){
+    if(!isEmpty()){
+        int temp = myQueue[0];
+        for(int i = 0 ;i<top-1; i++){
+            QueueArr[i] = QueueArr[i+1];
+        }
+        return temp;
+    } else {
+        throw 1;
+    }
+}
+*/
+
+Queue::Queue(){
+    top = -1;
+}
+
+Queue::~Queue(){
+}
+
+bool Queue::push(int id, string info){
+    bool valid = false;
+    if (top < QUEUE_SIZE - 1 && id > 0 && !info.empty()){
+        Data *myStruct = new Data;
+        myStruct->id = id;
+        myStruct->information = info;
+        top++;
+        myQueue[top] = myStruct;
+        valid = true;
+    }
+    return valid;  
+}
+
+bool Queue::pull(Data &queueData){
+    bool popped = false;
+    if(top != -1){
+        queueData.information = myQueue[0]->information;
+        queueData.id = myQueue[0]->id;
+        delete myQueue[0];
+        for(int i = 0 ;i<top-1; i++){
+            myQueue[i] = myQueue[i+1];
+        }
+        top--;
+        popped = true;
+    }
+    return popped;
+}
+
+bool Queue::peek(Data &queueData){
+    bool peeked = false;
+    if(top != -1){
+        queueData.information = myQueue[0]->information;
+        queueData.id = myQueue[0]->id;
+        peeked = true;
+    }
+    return peeked;
+}
+
+bool Queue::isEmpty(){
+    bool empty = true;
+    if(top >=0){
+        empty = false;
+    }
+    return empty;
+}
--- queue.h
+++ queue.h
@@ -7,20 +7,22 @@ Purpose: To create a bi-directional or undirected graph that is weighted, using 
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include "data.h"
+
 #define QUEUE_SIZE 15
 
 class Queue{
     public:
         Queue();
         ~Queue();
-        bool push(int num);
-        bool isEmpty();
-        int peek();
-        int pull();
 
+        bool push(int id, string info);
+        bool pull(Data &queueData);
+        bool peek(Data &queueData);
+        bool isEmpty();
     private:
         int top;
-        int StackArr[QUEUE_SIZE];
+        Data *myQueue[QUEUE_SIZE];
 };
 
 #endif
