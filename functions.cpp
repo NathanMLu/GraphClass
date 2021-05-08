b6701b87e2cbfa1707b71f2c6ee24e0f52057b1a
NathanMLu <nathanlumax@Gmail.com>
2021-05-08 01:23:17 -0500

    added extra bad case at the end

--- functions.cpp
+++ functions.cpp
@@ -9,8 +9,9 @@ Purpose: To create a bi-directional or undirected graph that is weighted, using 
 void makeTestCases(int *ids, int *weights, char** data, int size){
     int duplicate = (rand()%size)+1;
     string dup = "duplicate";
+    string neg = "negative weight";
     string bad = "bad data";
-    for(int i = 0 ; i < size-2; i++){
+    for(int i = 0 ; i < size-3; i++){
         ids[i] = rand() % MAX_ID +1; //creates random int from 1->1000
         if(i==duplicate){
             duplicate = ids[i];
@@ -21,16 +22,22 @@ void makeTestCases(int *ids, int *weights, char** data, int size){
         }
         data[i][BUFFER-1] = '\0'; //marks the end of the string, we stop 1 before buffer
     }
-    ids[size-2] = duplicate; //guarantees duplicate
+    ids[size-3] = duplicate; //guarantees duplicate
+    ids[size-2] = (rand() %size) +1;
     ids[size-1] = -1; //bad data
-    weights[size-2] = duplicate;
+    weights[size-3] = duplicate;
+    weights[size-2] = (rand() % 3) - 10;
     weights[size-1] = -1;
     for(int i=0; i <dup.size(); i++){
-        data[size-2][i] = dup.at(i);
+        data[size-3][i] = dup.at(i);
+    }
+    for(int i=0; i <neg.size(); i++){
+        data[size-2][i] = neg.at(i);
     }
     for(int i=0; i <bad.size(); i++){
         data[size-1][i] = bad.at(i);
     }
+
 }
 
 void displayTestCases(int* ids, int* weights, char** data, int size){
